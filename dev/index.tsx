0a1ffde4fb01854aad82a900d6672ec1eb1f4b15
IplayLF2 <914637118@qq.com>
2019-12-02 19:15:27 +0800

    不完善的

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,4 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { AssignBuilder, AssignNext } from "../src/assign-builder";
+
+const builder = AssignBuilder.create<{
+  a: string;
+}>()
+  .use((next: AssignNext<{ b: number }>) => context => {
+    console.log("m1->", JSON.stringify(context));
+    next({ b: 1 });
+    console.log("<-m1", JSON.stringify(context));
+  })
+  .use((next: AssignNext<{ c: boolean }>) => context => {
+    console.log("m2->", JSON.stringify(context));
+    next({ c: true });
+    console.log("<-m2", JSON.stringify(context));
+  });
+
+const run = builder.build();
+console.log("run");
+console.dir(run);
+console.dir(run({ a: "aaa" }));
+
 ReactDOM.render(<div>dev</div>, document.getElementById("root"));
--- src/assign-builder.ts
+++ src/assign-builder.ts
@@ -1,16 +1,58 @@
-export class AssignBuilder<T extends Head, Head> {
+export class AssignBuilder<T extends Head, Head extends object> {
+  static create<T extends object>(): AssignBuilder<T, T> {
+    return new AssignBuilder<T, T>(assignNextSlot => source => {
+      const context = Object.assign({}, source);
+      assignNextSlot(context)({} as any);
+      return context;
+    });
+  }
+
+  private constructor(mapSlot: MapSlot<any, T>) {
+    this.mapSlot = mapSlot;
+  }
+
   use<K>(
     middleware: AssignMiddleware<T, K>
   ): K extends object ? AssignBuilder<T & K, Head> : AssignBuilder<T, Head> {
-    throw "";
+    return new AssignBuilder<any, any>(
+      AssignBuilder.createMapSlot(this.mapSlot, middleware)
+    ) as any;
   }
-  builder(): (source: Head) => T {
-    throw "";
+
+  build(): (source: Head) => T {
+    return this.mapSlot(context => side => Object.assign(context, side));
   }
+
+  private static createAssignNextSlot(next: Next): AssignNextSlot<any, any> {
+    return context => side => {
+      Object.assign(context, side);
+      next(context);
+    };
+  }
+
+  private static createMapSlot(
+    nextMapSlot: MapSlot<any, any>,
+    middleware: AssignMiddleware<any, any>
+  ): MapSlot<any, any> {
+    return assignNextSlot => context =>
+      nextMapSlot(
+        AssignBuilder.createAssignNextSlot(middleware(assignNextSlot(context)))
+      )(context);
+  }
+
+  private mapSlot: MapSlot<any, T>;
 }
 
+export type AssignNext<T = undefined> = (side: T) => void;
+
 export type AssignMiddleware<T, K = undefined> = (
-  next: AssignNext<K>
+  assignNext: AssignNext<K>
 ) => (context: T) => void;
 
-export type AssignNext<T = undefined> = (assign: T) => void;
+type Next = (context: any) => void;
+
+type AssignNextSlot<T, K> = (context: T) => AssignNext<K>;
+
+type Map<T, K> = (source: T) => K;
+
+type MapSlot<T, K> = (assignNextSlot: AssignNextSlot<T, K>) => Map<T, T & K>;
