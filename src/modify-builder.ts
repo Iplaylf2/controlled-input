0c6e12508e3c1be3950c60692d3b2ca6620cfad5
IplayLF2 <914637118@qq.com>
2019-12-05 12:44:50 +0800

    add modify-builder

---
+++ src/modify-builder.ts
@@ -0,0 +1,51 @@
+export class ModifyBuilder<T> {
+  static create<T>(): ModifyBuilder<T> {
+    return new ModifyBuilder(next => source => next(source));
+  }
+
+  use(middleware: ModifyMiddleware<T>): ModifyBuilder<T> {
+    return new ModifyBuilder(
+      ModifyBuilder.createModfiyRunSlot(this.modifyRunSlot, middleware)
+    );
+  }
+
+  build(): (source: T) => T {
+    const modifyRun = this.modifyRunSlot(() => {});
+    return source => {
+      const context = ModifyBuilder.cloneContext(source);
+      modifyRun(context);
+      return context;
+    };
+  }
+
+  private static createModfiyRunSlot<T>(
+    modifyRunSlot: ModifyRunSlot<T>,
+    middleware: ModifyMiddleware<T>
+  ): ModifyRunSlot<T> {
+    return next => modifyRunSlot(middleware(next));
+  }
+
+  private static cloneContext<T>(context: T): T {
+    if (Array.isArray(context)) {
+      return context.slice(0) as any;
+    }
+    if (typeof context === "object") {
+      return Object.assign(
+        Object.create(Object.getPrototypeOf(context)),
+        context
+      );
+    }
+    return context;
+  }
+
+  private constructor(modifyRunSlot: ModifyRunSlot<T>) {
+    this.modifyRunSlot = modifyRunSlot;
+  }
+  private modifyRunSlot: ModifyRunSlot<T>;
+}
+
+export type ModifyNext<T> = (context: T) => void;
+export type ModifyMiddleware<T> = (next: ModifyNext<T>) => ModifyNext<T>;
+
+type ModifyRun<T> = (source: T) => void;
+type ModifyRunSlot<T> = (next: ModifyNext<T>) => ModifyRun<T>;
