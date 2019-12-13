6ab92e3dd68b2c5e025ba1c35af2bde40be6c475
IplayLF2 <914637118@qq.com>
2019-12-13 12:25:06 +0800

    complete adjust-detect

--- src/middleware/adjust-detect.ts
+++ src/middleware/adjust-detect.ts
@@ -16,12 +16,23 @@ export const AdjustDetect: ModifyMiddleware<InputNumberContext> = function(
       context.adjust = InputNumberAdjustType.Native;
     } else if (afterAdjust.type === InputChangeType.None) {
       context.adjust = InputNumberAdjustType.Block;
-    } else if (beforeAdjust.selectionTo > afterAdjust.selectionTo && false) {
+    } else if (
+      beforeAdjust.selectionTo > afterAdjust.selectionTo &&
+      testInclude(beforeAdjust.textTo, afterAdjust.textTo)
+    ) {
       context.adjust = InputNumberAdjustType.Trim;
-    } else if (beforeAdjust.selectionTo < afterAdjust.selectionTo && false) {
+    } else if (
+      beforeAdjust.selectionTo < afterAdjust.selectionTo &&
+      testInclude(afterAdjust.textTo, beforeAdjust.textTo)
+    ) {
       context.adjust = InputNumberAdjustType.Complete;
     } else {
       context.adjust = InputNumberAdjustType.Mutate;
     }
   };
+};
+
+const testInclude = function(full: string, part: string): boolean {
+  const regText = Array.from(part).reduce((r, c) => r + ".*" + c);
+  return new RegExp(regText).test(full);
 };
