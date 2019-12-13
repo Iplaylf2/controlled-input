0d617480dfff80c1e2b4700349a953547d28574e
IplayLF2 <914637118@qq.com>
2019-12-13 15:50:41 +0800

    improve normal-adjust

--- src/middleware/normal-adjust.ts
+++ src/middleware/normal-adjust.ts
@@ -10,8 +10,32 @@ export const NormalAdjust: ModifyMiddleware<InputNumberContext> = function(
     switch (change.type) {
       case InputChangeType.Append:
       case InputChangeType.Replace:
-        const afterFilter = change.changeTo.replace(/[^\-\.0-9]/g, "");
-        context.change.alterChangeTo(afterFilter);
+        const filterIllegalChar = change.changeTo.replace(/[^\-\.0-9]/g, "");
+
+        const filterMinus =
+          change.changeIndex === 0
+            ? filterIllegalChar.charAt(0) +
+              filterIllegalChar.substring(1).replace(/\-/g, "")
+            : filterIllegalChar.replace(/\-/g, "");
+
+        let filterDot: string;
+        const dotIndex =
+          change.textFrom.length - context.inputFrom.input.fractional.length;
+
+        if (
+          context.inputFrom.input.fractional.length > 0 &&
+          dotIndex < change.changeIndex &&
+          dotIndex >= change.selectionFrom
+        ) {
+          filterDot = filterMinus.replace(/\./g, "");
+        } else {
+          const border = filterMinus.indexOf(".") + 1;
+          filterDot =
+            filterMinus.substring(0, border) +
+            filterMinus.substring(border).replace(/\./g, "");
+        }
+
+        context.change.alterChangeTo(filterDot);
         if (context.change.type === InputChangeType.None) {
           return;
         }
