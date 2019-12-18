7a41d80d05450916f01e24dcd19fb0a69e79a7b9
IplayLF2 <914637118@qq.com>
2019-12-18 17:08:54 +0800

    fix ValidDetect

--- src/middleware/valid-detect.ts
+++ src/middleware/valid-detect.ts
@@ -5,19 +5,30 @@ export const ValidDetect: ModifyMiddleware<InputNumberContext> = function(
   next
 ) {
   return context => {
-    const { inputTo } = context;
-    if (Number.isNaN(inputTo.input.value)) {
+    const { inputTo } = context,
+      { value } = inputTo.input;
+    if (Number.isNaN(value)) {
       inputTo.valid = false;
     } else {
       const { config } = context;
-      const validValue = getVaildValue(
-        inputTo.input.value,
-        config.min,
-        config.max,
-        config.step,
-        config.precision
-      );
-      inputTo.valid = validValue === inputTo.input.value;
+      if (value < config.min) {
+        inputTo.valid = false;
+      } else if (value > config.max) {
+        inputTo.valid = false;
+      } else {
+        if (config.precision !== Infinity) {
+          const sign = Math.sign(value),
+            shim = Math.pow(10, config.precision),
+            shimValue = Math.round(Math.abs(value) * shim),
+            shimStep = Math.round(config.step * shim),
+            q = Math.floor(shimValue / shimStep);
+
+          const vaildValue = (sign * (q * shimStep)) / shim;
+          inputTo.valid = vaildValue === value;
+        } else {
+          inputTo.valid = true;
+        }
+      }
     }
     next(context);
   };
