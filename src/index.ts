62e9e2a6fdd0a6ae26adb294c049bc8f8a504b98
IplayLF2 <914637118@qq.com>
2019-12-18 17:09:10 +0800

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export default "start";
+export { ModifyBuilder, ModifyNext, ModifyMiddleware } from "./modify-builder";
+export {
+  InputNumberContext,
+  InputNumberConfig,
+  InputNumberAdjustType
+} from "./input-number-context";
+export { InputChange, InputChangeType } from "./input-change";
+export { InputNumber } from "./input-number";
+
+import * as _MiddlewareList from "./middleware";
+export const MiddlewareList = _MiddlewareList;
