2c910ae8f1f119dca951693e5b595f8823157225
IplayLF2 <914637118@qq.com>
2020-01-08 14:36:50 +0800

    show valid

--- dev/index.tsx
+++ dev/index.tsx
@@ -6,17 +6,26 @@ import { InputNumber } from "./input-number/input-number";
 const Root = function() {
   const [text, setText] = useState("");
   const [change, setChange] = useState("-");
+  const [valid, setValid] = useState(false);
 
   return (
     <div className="root">
       <InputNumber
+        config={{
+          max: Infinity,
+          min: -Infinity,
+          step: 0.5,
+          precision: 1
+        }}
         value={text}
         onChange={e => {
           setText(e.text);
           setChange(e.change);
+          setValid(e.valid);
         }}
       />
       <div>{change}</div>
+      <div>{String(valid)}</div>
     </div>
   );
 };
